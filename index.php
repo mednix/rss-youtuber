c6bb782d8db669ee991b6b2ff75ba30c08d2b9b3
Mohammed EL HADROUBI <med.programmer@gmail.com>
2015-06-17 20:27:51 +0000

    create the rss youtuber

--- index.php
+++ index.php
@@ -8,8 +8,10 @@ if(DEBUG==true){
 require 'vendor/autoload.php';
 
 $key='AIzaSyAhYKR543743GnhQmNwIhxULVinpftfIKU';
-$id='PL784A9CFA20E3E33C';
-
+$id=$_GET['playlist'];
+if(empty($id)){
+    $id='PL784A9CFA20E3E33C';
+}
 $client=new \RssYoutuber\Client($key);
 $playlist=new \RssYoutuber\Playlist($client);
 $rss=$playlist->id($id)
